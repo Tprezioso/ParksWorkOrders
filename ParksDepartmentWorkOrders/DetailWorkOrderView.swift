b1402c8eb30cbe01157d4947de27a42b5208298b
Tprezioso <tommyprezioso@gmail.com>
2020-11-03 15:19:50 -0500

    extacted subview for detail view

--- ParksDepartmentWorkOrders/DetailWorkOrderView.swift
+++ ParksDepartmentWorkOrders/DetailWorkOrderView.swift
@@ -13,61 +13,7 @@ struct DetailWorkOrderView: View {
     
     var body: some View {
         ZStack {
-            ScrollView {
-                Image("PlaceholderImage")
-                    .resizable()
-                    .scaledToFit()
-                    .padding()
-                
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("Work Order Completed:")
-                        .font(.title)
-                    Text("XX/XX/XXXX")
-                        .font(.title2)
-                    
-                    Text("Crew:")
-                        .font(.title3)
-                        .font(.subheadline)
-                    ForEach(names, id: \.self) { name in
-                        VStack {
-                            Text(name)
-                                .font(.subheadline)
-                        }
-                        
-                    }
-                    
-                    Text("Materials:")
-                        .font(.title3)
-                    
-                    ForEach(materials, id: \.self) { material in
-                        VStack {
-                            Text(material)
-                                .font(.subheadline)
-                        }
-                    }
-                    
-                    Text("Work Time:")
-                        .font(.title3)
-                    Text("1 hour")
-                        .font(.subheadline)
-                    
-                    Text("Travel Time:")
-                        .font(.title3)
-                    Text("40 mins")
-                        .font(.subheadline)
-                    
-                }.padding()
-                Image("PlaceholderImage")
-                    .resizable()
-                    .scaledToFit()
-                    .padding()
-                
-                Image("PlaceholderImage")
-                    .resizable()
-                    .scaledToFit()
-                    .padding()
-                
-            }
+            WorkOrderCompletedView(names: names, materials: materials)
         }
     }
 }
@@ -75,5 +21,67 @@ struct DetailWorkOrderView: View {
 struct DetailWorkOrderView_Previews: PreviewProvider {
     static var previews: some View {
         DetailWorkOrderView()
+    }
+}
+
+struct WorkOrderCompletedView: View {
+    var names = [""]
+    var materials = [""]
+    var body: some View {
+        ScrollView {
+            Image("PlaceholderImage")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Work Order Completed:")
+                    .font(.title)
+                Text("XX/XX/XXXX")
+                    .font(.title2)
+                
+                Text("Crew:")
+                    .font(.title3)
+                    .font(.subheadline)
+                ForEach(names, id: \.self) { name in
+                    VStack {
+                        Text(name)
+                            .font(.subheadline)
+                    }
+                    
+                }
+                
+                Text("Materials:")
+                    .font(.title3)
+                
+                ForEach(materials, id: \.self) { material in
+                    VStack {
+                        Text(material)
+                            .font(.subheadline)
+                    }
+                }
+                
+                Text("Work Time:")
+                    .font(.title3)
+                Text("1 hour")
+                    .font(.subheadline)
+                
+                Text("Travel Time:")
+                    .font(.title3)
+                Text("40 mins")
+                    .font(.subheadline)
+                
+            }.padding()
+            Image("PlaceholderImage")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            
+            Image("PlaceholderImage")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            
+        }
     }
 }
